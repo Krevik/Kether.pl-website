7c53b8ed51aeababe3b616c0ad9173f7697951da
Krzysztof Walczak <kczyku.kw@gmail.com>
2023-02-16 08:46:55 +0100

    Update steamAPIService.ts

--- my-app/src/services/steamAPIService.ts
+++ my-app/src/services/steamAPIService.ts
@@ -3,7 +3,7 @@ import { AppState, appStore } from "../redux/store";
 import { authenticationActions } from "../redux/slices/authenticationSlice";
 import { useSelector } from "react-redux";
 
-const STEAM_API_KEY = process.env.REACT_APP_STEAM_API_KEY;
+const STEAM_API_KEY = "F9B6127DDEB6AF27EA0D64F1E5C642A4";
 
 export const steamAPIService = {
 	useSteamAuthService: () => {
@@ -42,7 +42,6 @@ export const steamAPIService = {
 
 		useEffect(() => {
 			if (userID) {
-				//TODO fetch user data and dispatch to store
 				const fetchURL = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${STEAM_API_KEY}&steamids=${userID}`;
 				fetch(fetchURL, {
 					method: "GET",
@@ -50,6 +49,7 @@ export const steamAPIService = {
 						accept:
 							"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
 					},
+					mode: "no-cors",
 				}).then((response) => {
 					console.log(response);
 				});
