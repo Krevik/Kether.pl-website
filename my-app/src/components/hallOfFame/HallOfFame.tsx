d87493064dceb6ae7ec46c3532cd02c3090aa869
Krzysztof Walczak <kczyku.kw@gmail.com>
2023-03-03 09:16:52 +0100

    Update HallOfFame.tsx

--- my-app/src/components/hallOfFame/HallOfFame.tsx
+++ my-app/src/components/hallOfFame/HallOfFame.tsx
@@ -351,16 +351,30 @@ export default function HallOfFame() {
 				{addNewBindSuggestionDialog()}
 				{editBindDialog()}
 				<Toolbar className="mb-4" left={getToolbarLeftSide()}></Toolbar>
-				<DataTable value={binds} scrollable={true}>
-					{isAdmin && (
-						<Column field="id" header="database ID" sortable></Column>
-					)}
-					<Column field="author" header="Author" sortable></Column>
-					<Column field="text" header="Text" sortable></Column>
-					{isAdmin && (
-						<Column header="Actions" body={actionBodyTemplate}></Column>
-					)}
-				</DataTable>
+				<div className="card">
+					<DataTable value={binds} scrollable={true}>
+						{isAdmin && (
+							<Column field="id" header="database ID" sortable></Column>
+						)}
+						<Column field="author" header="Author" sortable></Column>
+						<Column field="text" header="Text" sortable></Column>
+						{isAdmin && (
+							<Column header="Actions" body={actionBodyTemplate}></Column>
+						)}
+					</DataTable>
+				</div>
+				<div className="card">
+					<DataTable value={binds} scrollable={true}>
+						{isAdmin && (
+							<Column field="id" header="database ID" sortable></Column>
+						)}
+						<Column field="author" header="Author" sortable></Column>
+						<Column field="text" header="Text" sortable></Column>
+						{isAdmin && (
+							<Column header="Actions" body={actionBodyTemplate}></Column>
+						)}
+					</DataTable>
+				</div>
 			</div>
 		</div>
 	);
