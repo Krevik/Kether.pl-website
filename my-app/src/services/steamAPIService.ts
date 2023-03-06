058719d60b5b9e465e6c55f474edff4e1339893a
Krzysztof Walczak <kczyku.kw@gmail.com>
2023-03-06 18:12:57 +0100

    Update steamAPIService.ts

--- my-app/src/services/steamAPIService.ts
+++ my-app/src/services/steamAPIService.ts
@@ -69,6 +69,7 @@ export const steamAPIService = {
 		useEffect(() => {
 			if (userID) {
 				steamAPIService.getUserData(userID).then((userData) => {
+					console.log(userData);
 					appStore.dispatch(userDataActions.setUserData(userData));
 				});
 			}
