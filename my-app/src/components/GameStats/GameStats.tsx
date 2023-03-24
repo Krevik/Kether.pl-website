28596ec9e7be328e1a4f2c004d253237762aeb43
Krzysztof Walczak <kczyku.kw@gmail.com>
2023-03-24 16:48:56 +0100

    Update GameStats.tsx

--- my-app/src/components/GameStats/GameStats.tsx
+++ my-app/src/components/GameStats/GameStats.tsx
@@ -77,9 +77,9 @@ export default function GameStats() {
         var m = Math.floor((d % 3600) / 60);
         var s = Math.floor((d % 3600) % 60);
 
-        var hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
-        var mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : '';
-        var sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : '';
+        var hDisplay = h + 'h';
+        var mDisplay = m + 'm';
+        var sDisplay = s + 's';
         return hDisplay + mDisplay + sDisplay;
     }
 
