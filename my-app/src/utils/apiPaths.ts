d6eb676c25eb3dcf9c7945414dff96588efcdaa0
Krzysztof Walczak <kczyku.kw@gmail.com>
2023-05-17 17:04:22 +0200

    Allow for local development

--- my-app/src/utils/apiPaths.ts
+++ my-app/src/utils/apiPaths.ts
@@ -1,8 +1,11 @@
+const isDevelopmentMode = false;
 export const apiPaths = {
-	API_DOMAIN: `https://kether-api.click`,
-	API_BASE_PATH: `/api`,
-	BINDS_PATH: `/binds`,
-	BIND_SUGGESTIONS_PATH: `/binds/suggestions`,
-	STEAM_PATH: `/steam`,
-	LIVE_SERVER_PATH: `/liveserver`,
+    API_DOMAIN: isDevelopmentMode
+        ? `http://localhost:3001`
+        : `https://kether-api.click`,
+    API_BASE_PATH: `/api`,
+    BINDS_PATH: `/binds`,
+    BIND_SUGGESTIONS_PATH: `/binds/suggestions`,
+    STEAM_PATH: `/steam`,
+    LIVE_SERVER_PATH: `/liveserver`,
 };
