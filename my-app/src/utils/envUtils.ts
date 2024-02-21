0dc1a340a36632601bc705b055db6fcc16de14d5
Dominik Adrian Grzywak <starterx4@gmail.com>
2024-02-21 04:59:01 +0100

    up api domain

--- my-app/src/utils/envUtils.ts
+++ my-app/src/utils/envUtils.ts
@@ -4,4 +4,4 @@ export const isDevelopmentEnvironment = () => {
 
 export const API_DOMAIN = isDevelopmentEnvironment()
     ? 'http://localhost:3001'
-    : 'https://kether-api.click';
+    : 'https://kether-api.fun';
