2c9cd99c4dacc626c2c1258879b4a0c06d1d1b0e
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-10-21 04:20:59 +0200

    PageLayout: Simplify `backgrounds.ts` helper function

--- src/components/PageLayout/PageBackground/backgrounds.ts
+++ src/components/PageLayout/PageBackground/backgrounds.ts
@@ -1,18 +1,8 @@
-import background1 from '../../../resources/backgrounds/background_1.jpg';
-import background2 from '../../../resources/backgrounds/background_2.jpg';
-import background3 from '../../../resources/backgrounds/background_3.jpg';
-import background4 from '../../../resources/backgrounds/background_4.jpg';
-import background5 from '../../../resources/backgrounds/background_5.jpg';
-import background6 from '../../../resources/backgrounds/background_6.jpg';
-
-const createBackgroundUrl = (imageLoc: string) => {
-    return imageLoc;
-};
 export const BACKGROUNDS = {
-    BACKGROUND_1: createBackgroundUrl(background1),
-    BACKGROUND_2: createBackgroundUrl(background2),
-    BACKGROUND_3: createBackgroundUrl(background3),
-    BACKGROUND_4: createBackgroundUrl(background4),
-    BACKGROUND_5: createBackgroundUrl(background5),
-    BACKGROUND_6: createBackgroundUrl(background6),
-};
+    BACKGROUND_1: require('../../../resources/backgrounds/background_1.jpg'),
+    BACKGROUND_2: require('../../../resources/backgrounds/background_2.jpg'),
+    BACKGROUND_3: require('../../../resources/backgrounds/background_3.jpg'),
+    BACKGROUND_4: require('../../../resources/backgrounds/background_4.jpg'),
+    BACKGROUND_5: require('../../../resources/backgrounds/background_5.jpg'),
+    BACKGROUND_6: require('../../../resources/backgrounds/background_6.jpg'),
+} as const;
